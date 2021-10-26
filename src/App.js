174915fb84e8dfbba7204dfbe738b43b33fe9012
kylexrose@gmail.com <kylexrose@gmail.com>
2021-10-26 12:44:25 -0400

    detatched weather app link

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       </div>
       <section className="section section-dark">
       </section>
-      <div className="pimg2">
+      <div id="experience" className="pimg2">
         <div className="summary-text">
           <div className="experience text-background">
             <span className="title">Full Stack (MERN) Web Developer</span>
@@ -48,11 +48,12 @@ function App() {
                 </div> */}
               </div>
             </a>
-            <a className="project-window" href="https://weather.kylexrose.com/" target="_blank">
+            <a className="project-window" href="#" target="_blank">
               <img className="project-preview" src={weather_preview} alt="weather app preview"/>
               <div className="project-text">
                 <p className="project-title">Weather.com Facsimile</p>
-                <p className="project-summary">A copy of a weather.com page using only JavaScript, html, some Bootstrap, and CSS</p>
+                <p className="project-summary">A copy of a weather.com page using only JavaScript, html, some Bootstrap, and CSS.</p><br/>
+                <p>Preview Coming Soon</p>
                 {/* <img className="git" alt="github link" src={gitImage}/> */}
               </div>
             </a>
